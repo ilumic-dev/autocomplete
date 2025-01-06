647e938f93cb8dadcc67a25dad108b4be52bae1b
pico190 <saulbuesosanz@gmail.com>
2025-01-06 17:45:51 +0100

    its not working :)

--- php/isCursorInPhp.js
+++ php/isCursorInPhp.js
@@ -1,11 +1,8 @@
 window.phpIntellisense.isCursorInPHP = function (code, cursorPosition) {
-    const { line, column } = cursorPosition;
+    const line = cursorPosition.line;
+    const column = cursorPosition.column;
 
     const lines = code.split("\n");
-
-    if (line < 1 || line > lines.length) {
-        throw new Error("La posición de la línea está fuera del rango del código.");
-    }
 
     let absolutePosition = 0;
     for (let i = 0; i < line - 1; i++) {
@@ -18,16 +15,22 @@ window.phpIntellisense.isCursorInPHP = function (code, cursorPosition) {
     const phpBlocks = [];
 
     while ((match = phpRegex.exec(code)) !== null) {
-        if (match[0] === "<?php" || match[0] === "<?") {
+        if (match[0] === '<?php' || match[0] === '<?') {
             phpBlocks.push({ start: match.index });
-        } else if (match[0] === "?>" && phpBlocks.length > 0) {
+        } else if (match[0] === '?>' && phpBlocks.length > 0) {
             phpBlocks[phpBlocks.length - 1].end = match.index;
         }
     }
 
+    // Si hay bloques sin cerrar, considera que se extienden hasta el final del código
     for (const block of phpBlocks) {
-        const end = block.end ?? code.length;
-        if (block.start <= absolutePosition && absolutePosition < end) {
+        if (block.end === undefined) {
+            block.end = code.length;
+        }
+    }
+
+    for (const block of phpBlocks) {
+        if (block.start <= absolutePosition && absolutePosition < block.end) {
             return true;
         }
     }
